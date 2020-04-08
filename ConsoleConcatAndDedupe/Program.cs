b11df89e076dad8839b3e0f0c4dbf57ed0350ce9
TedKuehn <kuehnt831@hotmail.com>
2020-04-07 23:27:20 -0500

    initial commit

---
+++ ConsoleConcatAndDedupe/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleConcatAndDedupe
+{
+    class MergeNames
+    {
+        public static string[] UniqueNames(string[] names1, string[] names2)
+        {
+            string[] namesResult = names1.Concat(names2).ToArray();
+            IEnumerable<string> distinctNames = namesResult.Distinct();
+            return distinctNames.ToArray();
+        }
+        
+        public static void Main(string[] args)
+        {
+            string[] names1 = new string[] {"Ava", "Emma", "Olivia", "Ava"};
+            string[] names2 = new string[] {"Olivia", "Sophia", "Emma", "Julie", "Julie"};
+            Console.WriteLine(string.Join(", ", MergeNames.UniqueNames(names1, names2))); // should print Ava, Emma, Olivia, Sophia
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+### Concat and Depupe
+Setup
+* make sure .NET Core SDK is installed to run dotnet CLI commands
+
+`dotnet run --project ConsoleConcatAndDedupe/ConsoleConcatAndDedupe.csproj //runs the sample`
+
